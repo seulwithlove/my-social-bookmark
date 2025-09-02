fab4ed9cd0dada9dcd8ad3c6b7263f1eb6414032
Lee Seul <love.and.seul@gmail.com>
2025-09-02 18:59:04 +0900

    feat: My page UI

--- app/my/page.tsx
+++ app/my/page.tsx
@@ -1,3 +1,18 @@
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+
 export default function My() {
-  return <div>My</div>;
+  return (
+    <div className="grid place-items-center">
+      <div className="w-96 border p-5 text-center">
+        <h1 className="mb-5 text-3xl">My</h1>
+        <div className="flex justify-around gap-3">
+          <Link className={"text-btn"} href="/api/auth/signout">
+            Go to SignOut
+          </Link>
+          <Button variant={"info"}>Sign Out</Button>
+        </div>
+      </div>
+    </div>
+  );
 }
