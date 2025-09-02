6903f39db8841b484821af23358eff011be96303
Lee Seul <love.and.seul@gmail.com>
2025-09-02 18:59:38 +0900

    feat: UI set based on session

--- app/nav.tsx
+++ app/nav.tsx
@@ -1,20 +1,28 @@
 import { BookHeartIcon } from "lucide-react";
 import Link from "next/link";
+import { use } from "react";
 import ThemeChanger from "@/components/theme-changer";
+import { auth } from "@/lib/auth";
 
 export default function Nav() {
+  const session = use(auth());
+  const didLogin = !!session?.user;
+  console.log("💻 ~ session:", session);
   return (
     <div className="flex items-center gap-5">
       <Link href={"/bookcase"} className="btn-icon">
         <BookHeartIcon />
       </Link>
       <ThemeChanger />
-      <Link href="/my" className={"text-btn"}>
-        My
-      </Link>
-      <Link href="/sign" className={"text-btn"}>
-        Login
-      </Link>
+      {didLogin ? (
+        <Link href="/my" className={"text-btn"}>
+          {session.user?.name}
+        </Link>
+      ) : (
+        <Link href="/sign" className={"text-btn"}>
+          Login
+        </Link>
+      )}
     </div>
   );
 }
