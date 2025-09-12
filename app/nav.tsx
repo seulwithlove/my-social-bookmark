163c634f60eae7fe62e27b6cbbc01c95ff3da626
Lee Seul <love.and.seul@gmail.com>
2025-09-12 21:32:57 +0900

    feat: add profile image to nav
    - configure remote images

--- app/nav.tsx
+++ app/nav.tsx
@@ -1,8 +1,10 @@
 import { BookHeartIcon } from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
 import { use } from "react";
 import ThemeChanger from "@/components/theme-changer";
 import { auth } from "@/lib/auth";
+import DummyProfile from "@/public/profile_dummy.png";
 
 export default function Nav() {
   const session = use(auth());
@@ -14,8 +16,13 @@ export default function Nav() {
       </Link>
       <ThemeChanger />
       {didLogin ? (
-        <Link href="/my" className={"text-btn"}>
-          {session.user?.name}
+        <Link href="/my" className={"overflow-hidden rounded-full text-btn"}>
+          <Image
+            src={session.user?.image || DummyProfile}
+            alt={session.user?.name || "guest"}
+            width={40}
+            height={40}
+          />
         </Link>
       ) : (
         <Link href="/sign" className={"text-btn"}>
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      { hostname: "*.googleusercontent.com" },
+      { hostname: "avatars.githubusercontent.com" },
+      { hostname: "phinf.pstatic.net" },
+      { hostname: "*.kakaocdn.net" },
+      { hostname: "localhost" },
+    ],
+  },
 };
 
 export default nextConfig;
