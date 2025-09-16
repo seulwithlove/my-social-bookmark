58ce0420139baacdcc9586b3b532b074fe495854
Lee Seul <love.and.seul@gmail.com>
2025-09-16 17:02:36 +0900

    feat: zod-based validation for credentials
    - create the database of new members

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,9 +1,12 @@
-import NextAuth from "next-auth";
+import NextAuth, { AuthError, type User } from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 import Github from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
 import Kakao from "next-auth/providers/kakao";
 import Naver from "next-auth/providers/naver";
+import z from "zod";
+import { findMemberByEmail } from "@/app/sign/sign.action";
+import prisma from "./db";
 
 export const {
   handlers: { GET, POST },
@@ -23,21 +26,48 @@ export const {
         passwd: {},
       },
       async authorize(credentials) {
-        console.log("credentials>>", credentials);
-        return null;
+        // console.log("credentials>>", credentials);
+        const { email, passwd } = credentials;
+        const validator = z
+          .object({
+            email: z.email("Wrong email address!"),
+            passwd: z.string().min(6, "More than 6 characters!"),
+          })
+          .safeParse({ email, passwd });
+
+        if (!validator.success) {
+          console.log("Error:", validator.error);
+          throw new AuthError(validator.error.message);
+        }
+        return { email, passwd } as User;
       },
     }),
   ], // TODO: GET / POST
   callbacks: {
     async signIn({ user, profile, account }) {
       const isCredential = account?.provider === "credentials";
-      console.log("💻 ~ isCredential:", isCredential);
-      console.log("💻 ~ profile:", profile);
-      console.log("💻 ~ user:", user);
+      // console.log("💻 ~ isCredential:", isCredential);
+      // console.log("💻 ~ profile:", profile);
+      // console.log("💻 ~ user:", user);
 
       const { email, name: nickname, image } = user;
       if (!email) return false;
 
+      const mbr = await findMemberByEmail(email, isCredential);
+
+      if (mbr?.emailcheck) {
+        return `/sign/error?error=CheckEmail&email=${email}`;
+      }
+
+      if (isCredential) {
+        if (!mbr) throw new AuthError("NotExistsMember");
+      } else {
+        if (!mbr && nickname) {
+          await prisma.member.create({
+            data: { email, nickname, image },
+          });
+        }
+      }
       return true;
     },
     async jwt({ token, user, trigger, account, session }) {
@@ -50,6 +80,7 @@ export const {
       }
       return token;
     },
+
     async session({ session, token }) {
       if (token) {
         session.user.id = token.id?.toString() || "";
@@ -59,6 +90,7 @@ export const {
       return session;
     },
   },
+
   trustHost: true, // TODO: crs 문제 해결?
   jwt: { maxAge: 30 * 60 }, // 30mins
   pages: {
